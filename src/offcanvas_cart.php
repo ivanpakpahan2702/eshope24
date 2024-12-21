bbbd5db5434c638b24086ba9d9ac266800130b7a
ivanpakpahan27 <ivanpakpahangn@gmail.com>
2024-12-21 22:05:01 +0700

    Cart Progress

--- src/offcanvas_cart.php
+++ src/offcanvas_cart.php
@@ -36,18 +36,32 @@ if (mysqli_num_rows($result) > 0) {
                     </p>
 
                     <div class="d-flex mb-4">
-                        <label for="sub_total" class="me-2">Jumlah (Kg)</label>
-                        <button id="<?php echo $row['id'] ?>" class="btn minus cart-btn-minus me-2">-</button>
+                        <label for="sub_total" class="form-label me-2">Jumlah (Kg)</label>
+                        <button id="<?php echo $row['id'] ?>" class="btn minus btn-danger cart-btn-minus me-2">-</button>
                         <input class="form-control" id="sub_total" type="text" value="<?php echo $row['sub_total']; ?>" style="text-align: center;" min="0" max="<?php echo $row['stock']; ?>" disabled />
-                        <button id="<?php echo $row['id'] ?>" class="btn ms-2 cart-btn">+</button>
+                        <button id="<?php echo $row['id'] ?>" class="btn ms-2 btn-success cart-btn">+</button>
                     </div>
 
                 </div>
             </div>
         </li>
-<?php
+    <?php
     }
-    echo '</ul>';
+    $sql2 = "
+    SELECT SUM(products_tbl.price) AS total_harga
+    FROM cart_items_tbl
+    JOIN products_tbl ON cart_items_tbl.product_id = products_tbl.id WHERE cart_items_tbl.user_id=1;
+    ";
+    $result2 = mysqli_query($conn, $sql2);
+    $data2 = mysqli_fetch_assoc($result2);
+    ?>
+    </ul>
+    <div class="d-flex mb-5 p-2">
+        <label for="total-transaction" class="form-label me-5">Total</label>
+        <input id="total-transaction" type="text" class="form-control" disabled style="text-align: center;" value="<?php echo $data2['total_harga']; ?>">
+    </div>
+    <button class="btn btn-outline-primary">Checkout</button>
+<?php
 } else {
-    echo 'Anda tidak memiliki Wishlist';
+    echo 'Anda tidak memiliki barang belanja';
 }
