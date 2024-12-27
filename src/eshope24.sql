47d406ef5195d68149d9f39c609c924f1c11a819
ivanpakpahan27 <ivanpakpahangn@gmail.com>
2024-12-27 14:13:22 +0700

    91%

--- src/eshope24.sql
+++ src/eshope24.sql
@@ -23,39 +23,93 @@ CREATE TABLE IF NOT EXISTS `cart_items_tbl` (
   `user_id` int(11) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `status_transaction` int(11) DEFAULT NULL,
-  `status_changed_date` timestamp NULL DEFAULT NULL,
+  `order_id` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_cart_items_tbl_products_tbl` (`product_id`),
   KEY `FK_cart_items_tbl_users_tbl` (`user_id`),
+  KEY `FK_cart_items_tbl_orders_tbl` (`order_id`),
+  CONSTRAINT `FK_cart_items_tbl_orders_tbl` FOREIGN KEY (`order_id`) REFERENCES `orders_tbl` (`id`),
   CONSTRAINT `FK_cart_items_tbl_products_tbl` FOREIGN KEY (`product_id`) REFERENCES `products_tbl` (`id`),
   CONSTRAINT `FK_cart_items_tbl_users_tbl` FOREIGN KEY (`user_id`) REFERENCES `users_tbl` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=132 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table eshope24.cart_items_tbl: ~8 rows (approximately)
+-- Dumping data for table eshope24.cart_items_tbl: ~30 rows (approximately)
 /*!40000 ALTER TABLE `cart_items_tbl` DISABLE KEYS */;
-INSERT INTO `cart_items_tbl` (`id`, `product_id`, `user_id`, `created_at`, `updated_at`, `status_transaction`, `status_changed_date`) VALUES
-	(1, 3, 1, '2024-12-21 19:37:32', '2024-12-21 19:37:32', NULL, NULL),
-	(3, 2, 1, '2024-12-21 19:51:03', '2024-12-21 19:51:03', NULL, NULL),
-	(4, 2, 1, '2024-12-21 19:56:28', '2024-12-21 19:56:28', NULL, NULL),
-	(5, 3, 1, '2024-12-21 20:07:49', '2024-12-21 20:07:49', NULL, NULL),
-	(6, 3, 1, '2024-12-21 20:07:52', '2024-12-21 20:07:52', NULL, NULL),
-	(7, 3, 1, '2024-12-21 20:18:11', '2024-12-21 20:18:11', NULL, NULL);
+INSERT INTO `cart_items_tbl` (`id`, `product_id`, `user_id`, `created_at`, `updated_at`, `order_id`) VALUES
+	(31, 2, 1, '2024-12-22 19:47:11', '2024-12-22 19:47:11', NULL),
+	(34, 1, 13, '2024-12-24 22:00:35', '2024-12-26 21:52:07', '1198932476'),
+	(35, 1, 13, '2024-12-24 22:08:36', '2024-12-26 21:52:07', '1198932476'),
+	(39, 2, 13, '2024-12-24 22:14:26', '2024-12-26 21:52:07', '1198932476'),
+	(55, 1, 36, '2024-12-24 22:16:42', '2024-12-24 22:16:42', NULL),
+	(73, 1, 36, '2024-12-24 22:20:22', '2024-12-24 22:20:22', NULL),
+	(74, 1, 36, '2024-12-24 22:20:25', '2024-12-24 22:20:25', NULL),
+	(75, 1, 36, '2024-12-24 22:20:26', '2024-12-24 22:20:26', NULL),
+	(76, 1, 36, '2024-12-24 22:20:26', '2024-12-24 22:20:26', NULL),
+	(77, 1, 36, '2024-12-24 22:20:26', '2024-12-24 22:20:26', NULL),
+	(78, 1, 36, '2024-12-24 22:20:26', '2024-12-24 22:20:26', NULL),
+	(79, 1, 36, '2024-12-24 22:20:28', '2024-12-24 22:20:28', NULL),
+	(80, 1, 36, '2024-12-24 22:20:28', '2024-12-24 22:20:28', NULL),
+	(81, 1, 36, '2024-12-24 22:22:22', '2024-12-24 22:22:22', NULL),
+	(82, 1, 36, '2024-12-24 22:22:23', '2024-12-24 22:22:23', NULL),
+	(83, 1, 36, '2024-12-24 22:22:24', '2024-12-24 22:22:24', NULL),
+	(84, 1, 36, '2024-12-24 22:22:25', '2024-12-24 22:22:25', NULL),
+	(85, 1, 36, '2024-12-24 22:22:26', '2024-12-24 22:22:26', NULL),
+	(86, 1, 36, '2024-12-24 22:22:27', '2024-12-24 22:22:27', NULL),
+	(87, 1, 36, '2024-12-24 22:22:27', '2024-12-24 22:22:27', NULL),
+	(88, 1, 36, '2024-12-24 22:25:32', '2024-12-24 22:25:32', NULL),
+	(94, 1, 36, '2024-12-24 22:36:18', '2024-12-24 22:36:18', NULL),
+	(101, 2, 13, '2024-12-26 18:41:34', '2024-12-26 21:52:07', '1198932476'),
+	(104, 1, 38, '2024-12-26 20:58:25', '2024-12-26 22:06:38', '2130428572'),
+	(105, 1, 38, '2024-12-26 20:58:32', '2024-12-26 22:06:38', '2130428572'),
+	(106, 3, 38, '2024-12-26 20:58:37', '2024-12-26 22:06:38', '2130428572'),
+	(107, 2, 13, '2024-12-26 21:46:15', '2024-12-26 21:52:07', '1198932476'),
+	(108, 1, 38, '2024-12-26 22:04:23', '2024-12-26 22:06:38', '2130428572'),
+	(109, 2, 38, '2024-12-26 22:04:32', '2024-12-26 22:06:38', '2130428572'),
+	(110, 1, 38, '2024-12-26 22:06:22', '2024-12-26 22:06:38', '2130428572'),
+	(111, 1, 39, '2024-12-27 10:38:28', '2024-12-27 13:17:01', '302831837'),
+	(112, 2, 39, '2024-12-27 10:38:31', '2024-12-27 13:17:01', '302831837'),
+	(113, 1, 39, '2024-12-27 11:07:55', '2024-12-27 13:17:01', '302831837'),
+	(114, 1, 39, '2024-12-27 11:08:57', '2024-12-27 13:17:01', '302831837'),
+	(115, 2, 39, '2024-12-27 13:16:47', '2024-12-27 13:17:01', '302831837'),
+	(129, 1, 39, '2024-12-27 14:02:37', '2024-12-27 14:02:37', NULL),
+	(130, 1, 13, '2024-12-27 14:08:49', '2024-12-27 14:08:49', NULL),
+	(131, 1, 13, '2024-12-27 14:11:46', '2024-12-27 14:11:46', NULL);
 /*!40000 ALTER TABLE `cart_items_tbl` ENABLE KEYS */;
 
--- Dumping structure for table eshope24.checkouts_tbl
-CREATE TABLE IF NOT EXISTS `checkouts_tbl` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `total` int(11) DEFAULT NULL,
-  `status` varchar(50) DEFAULT NULL,
+-- Dumping structure for table eshope24.orders_tbl
+CREATE TABLE IF NOT EXISTS `orders_tbl` (
+  `id` varchar(50) NOT NULL DEFAULT '',
+  `token` varchar(255) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `status` int(1) NOT NULL DEFAULT 0,
   `created_at` timestamp NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`id`)
+  `address` varchar(500) DEFAULT NULL,
+  `postal_code` varchar(50) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_checkouts_tbl_users_tbl` (`user_id`),
+  CONSTRAINT `FK_checkouts_tbl_users_tbl` FOREIGN KEY (`user_id`) REFERENCES `users_tbl` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table eshope24.checkouts_tbl: ~0 rows (approximately)
-/*!40000 ALTER TABLE `checkouts_tbl` DISABLE KEYS */;
-/*!40000 ALTER TABLE `checkouts_tbl` ENABLE KEYS */;
+-- Dumping data for table eshope24.orders_tbl: ~8 rows (approximately)
+/*!40000 ALTER TABLE `orders_tbl` DISABLE KEYS */;
+INSERT INTO `orders_tbl` (`id`, `token`, `user_id`, `status`, `created_at`, `updated_at`, `address`, `postal_code`, `name`, `phone`, `email`) VALUES
+	('1198932476', 'ecf41f91-c374-447c-9dd8-101d2aa6e58e', 13, 0, '2024-12-26 21:52:07', '2024-12-26 21:52:07', '', '', 'TestUserInvi', 'ds', 'ivanime027@gmail.com'),
+	('1338880016', 'dfb9961f-9425-4d3b-9a6f-40d0a9daee71', 13, 0, '2024-12-26 21:37:20', '2024-12-26 21:37:20', '', '', 'Ivan Pakpahan', '085268018218', 'ivanpakpahanchrst@gmail.com'),
+	('1342519321', '3cc8817f-5757-4062-9054-5e50f1e9807b', 13, 0, '2024-12-26 21:30:20', '2024-12-26 21:30:20', '', '', 'Ivan Pakpahan', '\r\n085268018218', 'ivanpakpahanchrst@gmail.com'),
+	('1537723519', '0ef7daff-e7bc-4dc5-bf25-ccc519c5e43e', 39, 0, '2024-12-27 11:08:35', '2024-12-27 11:08:35', '', '', 'IVAN PAKPAHAN', '000888', 'ivanpakpahanchrst@gmail.com'),
+	('1544825296', 'be672c49-82e8-4114-8427-a8dc97f69104', 38, 0, '2024-12-26 22:02:11', '2024-12-26 22:02:11', '', '', 'Dus', 'Dus', 'dus@mail.co'),
+	('1601571473', '78bce78a-9cdd-481f-8ad3-bddc3a901bab', 38, 0, '2024-12-26 22:04:47', '2024-12-26 22:04:47', '', '', 'IVAN PAKPAHAN', '44', 'ivanpakpahanchrst@gmail.com'),
+	('1772392754', '7c5f8930-0ecb-43ed-aad1-d7af23e02127', 13, 0, '2024-12-26 21:39:09', '2024-12-26 21:39:09', '', '', 'Ivan Pakpahan', '085268018218', 'ivanpakpahanchrst@gmail.com'),
+	('2060832721', '69802320-0c74-4c72-a8b8-493fcfeb138d', 39, 0, '2024-12-27 11:09:08', '2024-12-27 11:09:08', '', '', 'IVAN PAKPAHAN', '000888', 'ivanpakpahanchrst@gmail.com'),
+	('2130428572', 'b246780c-78c0-4094-8ae9-a6b623e800a8', 38, 0, '2024-12-26 22:06:38', '2024-12-26 22:06:38', '', '', 'Bli', '123', 'ivanpakpahanchrst@gmail.com'),
+	('302831837', 'b10b02f2-4c5f-4e18-8a9d-b64ca9a7e1c6', 39, 0, '2024-12-27 13:17:00', '2024-12-27 13:17:00', '', '', 'IVAN PAKPAHAN', '000888', 'ivanpakpahanchrst@gmail.com'),
+	('557490870', '6e409cc6-de62-4f94-87a4-1809b8da3498', 39, 0, '2024-12-27 10:39:09', '2024-12-27 10:39:09', 'Bengkulu', '37656', 'IVAN PAKPAHAN', '082345334444', 'ivanpakpahanchrst@gmail.com'),
+	('577274148', '3c48efda-eeb1-4756-a14b-45eba34945dd', 13, 0, '2024-12-26 21:28:44', '2024-12-26 21:28:44', '', '', 'Ivan Pakpahan', '\r\n085268018218', 'ivanpakpahanchrst@gmail.com');
+/*!40000 ALTER TABLE `orders_tbl` ENABLE KEYS */;
 
 -- Dumping structure for table eshope24.products_tbl
 CREATE TABLE IF NOT EXISTS `products_tbl` (
@@ -85,18 +139,26 @@ INSERT INTO `products_tbl` (`id`, `name`, `description`, `price`, `stock`, `imag
 -- Dumping structure for table eshope24.users_tbl
 CREATE TABLE IF NOT EXISTS `users_tbl` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) DEFAULT NULL,
-  `password` varchar(50) DEFAULT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
   `name` varchar(50) DEFAULT NULL,
   `address` varchar(50) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table eshope24.users_tbl: ~0 rows (approximately)
+-- Dumping data for table eshope24.users_tbl: ~7 rows (approximately)
 /*!40000 ALTER TABLE `users_tbl` DISABLE KEYS */;
 INSERT INTO `users_tbl` (`id`, `username`, `password`, `name`, `address`, `email`) VALUES
-	(1, 'ivan', '123', 'Ivan', 'Bengkulu', 'ivanpakpahan@mail.com');
+	(1, 'ivan', '123', 'Ivan', 'Bengkulu', 'ivanpakpahan@mail.com'),
+	(11, 'raka', '$2y$10$IYmOpe4GsstXgbaxpIMc4uDwUNhby2ogq2k7ISIAGS219u6ja3ccm', 'raka', NULL, NULL),
+	(13, 'user', '$2y$10$dmzD2JRcs8IbEsXiwsseA.tUbDOVPgle01utA4dkwoa.khhaLqQuy', 'user', NULL, NULL),
+	(35, 'sun', '$2y$10$Zl/gptfwlMlOZuGFjvyhuOWh1Q0BsxrdNVeiZSaUN.weajB5xh2gy', 'Sun', NULL, NULL),
+	(36, 'navi', '$2y$10$Zd6BWE4hgQ2YNcUGuQub2eOwWjUhMfTQuNYvkGJ70R86dbir38Us2', 'navi', NULL, NULL),
+	(37, 'van', '$2y$10$2nKmkPExj5wHXDjuVMo6.euj5F9O8micceoeFImEqAwKa6gsH8XUO', 'van', NULL, NULL),
+	(38, 'bill', '$2y$10$K9VOVB36afWk8PKSO7OseOK39T1uc2y2lLj1WnFG80CJaNfupeGJK', 'bill', NULL, NULL),
+	(39, 'bil', '$2y$10$jwhqvGaVWRed1BQF52BVyeIXkGWsbE/llUj488o8ksn5C5VTxH8be', 'bil', NULL, NULL);
 /*!40000 ALTER TABLE `users_tbl` ENABLE KEYS */;
 
 -- Dumping structure for table eshope24.wishlist_items_tbl
@@ -111,12 +173,23 @@ CREATE TABLE IF NOT EXISTS `wishlist_items_tbl` (
   KEY `FK_wishlist_items_tbl_users_tbl` (`user_id`),
   CONSTRAINT `FK__products_tbl` FOREIGN KEY (`product_id`) REFERENCES `products_tbl` (`id`),
   CONSTRAINT `FK_wishlist_items_tbl_users_tbl` FOREIGN KEY (`user_id`) REFERENCES `users_tbl` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=82 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table eshope24.wishlist_items_tbl: ~1 rows (approximately)
+-- Dumping data for table eshope24.wishlist_items_tbl: ~8 rows (approximately)
 /*!40000 ALTER TABLE `wishlist_items_tbl` DISABLE KEYS */;
 INSERT INTO `wishlist_items_tbl` (`id`, `product_id`, `created_at`, `updated_at`, `user_id`) VALUES
-	(46, 3, '2024-12-21 19:37:16', '2024-12-21 19:37:16', 1);
+	(47, 1, '2024-12-24 13:51:46', '2024-12-24 13:51:46', 1),
+	(59, 1, '2024-12-24 18:47:49', '2024-12-24 18:47:49', 36),
+	(67, 6, '2024-12-24 20:54:23', '2024-12-24 20:54:23', 36),
+	(68, 5, '2024-12-24 20:57:49', '2024-12-24 20:57:49', 36),
+	(69, 3, '2024-12-24 20:59:30', '2024-12-24 20:59:30', 36),
+	(74, 1, '2024-12-26 17:32:27', '2024-12-26 17:32:27', 13),
+	(76, 7, '2024-12-26 17:33:09', '2024-12-26 17:33:09', 13),
+	(77, 4, '2024-12-26 17:33:12', '2024-12-26 17:33:12', 13),
+	(78, 2, '2024-12-27 13:56:31', '2024-12-27 13:56:31', 13),
+	(79, 1, '2024-12-27 14:01:55', '2024-12-27 14:01:55', 39),
+	(80, 2, '2024-12-27 14:01:56', '2024-12-27 14:01:56', 39),
+	(81, 3, '2024-12-27 14:01:58', '2024-12-27 14:01:58', 39);
 /*!40000 ALTER TABLE `wishlist_items_tbl` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
--- src/offcanvas_cart.php
+++ src/offcanvas_cart.php
@@ -23,87 +23,87 @@ if ($result->num_rows > 0) {
     echo '<ul class="list-group">';
     while ($row = $result->fetch_assoc()) {
 ?>
-<li class="list-group-item">
-    <div class="card">
-        <input type="hidden"
-            name="product_id_cart"
-            value="checkout-<?php echo $row['id']; ?>">
-        <img src="assets/products/<?php echo $row['product_image'] ?>"
-            class="card-img-top"
-            alt="gambar produk" />
-        <div class="card-body p-2">
-            <h5 class="card-title">
-                <?php echo $row['product_name']; ?>
-            </h5>
-            <p class="card-text">
-                <hr />
-                <small><strong><?php echo "Rp" . number_format(($row['price']), 2, ",", ".") ?></strong></small>
-            </p>
-            <div class="d-flex mb-4">
-                <label for="sub_total"
-                    class="form-label me-2">Jumlah
-                    (Kg)</label>
-                <button
-                    id="cart-<?php echo $row['id'] ?>"
-                    class="btn minus btn-danger cart-btn-minus me-2">-</button>
-                <input class="form-control"
-                    name="total_product"
-                    type="text"
-                    value="<?php echo $row['total_product']; ?>"
-                    style="text-align: center;"
-                    disabled />
-                <button
-                    id="cart-<?php echo $row['id'] ?>"
-                    class="btn ms-2 btn-success cart-btn-plus">+</button>
+        <li class="list-group-item">
+            <div class="card">
+                <input type="hidden"
+                    name="product_id_cart"
+                    value="checkout-<?php echo $row['id']; ?>">
+                <img src="assets/products/<?php echo $row['product_image'] ?>"
+                    class="card-img-top"
+                    alt="gambar produk" />
+                <div class="card-body p-2">
+                    <h5 class="card-title">
+                        <?php echo $row['product_name']; ?>
+                    </h5>
+                    <p class="card-text">
+                        <hr />
+                        <small><strong><?php echo "Rp" . number_format(($row['price']), 2, ",", ".") ?></strong></small>
+                    </p>
+                    <div class="d-flex mb-4">
+                        <label for="sub_total"
+                            class="form-label me-2">Jumlah
+                            (Kg)</label>
+                        <button
+                            id="cart-<?php echo $row['id'] ?>"
+                            class="btn minus btn-danger cart-btn-minus me-2">-</button>
+                        <input class="form-control"
+                            name="total_product"
+                            type="text"
+                            value="<?php echo $row['total_product']; ?>"
+                            style="text-align: center;"
+                            disabled />
+                        <button
+                            id="cart-<?php echo $row['id'] ?>"
+                            class="btn ms-2 btn-success cart-btn-plus">+</button>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-</li>
-<?php
+        </li>
+    <?php
     }
     $sql = "
     SELECT SUM(products_tbl.price) AS total_harga
     FROM cart_items_tbl
-    JOIN products_tbl ON cart_items_tbl.product_id = products_tbl.id WHERE cart_items_tbl.user_id=$user_id;
+    JOIN products_tbl ON cart_items_tbl.product_id = products_tbl.id WHERE cart_items_tbl.user_id=$user_id AND cart_items_tbl.order_id IS NULL;
     ";
     $result = $conn->query($sql);
     $row = $result->fetch_assoc();
     ?>
-</ul>
-<div class="d-flex mb-5 p-2">
-    <label for="total-transaction"
-        class="form-label me-5">Total</label>
-    <input id="total-transaction" type="text"
-        class="form-control" disabled
-        style="text-align: center;"
-        value="<?php echo "Rp" . number_format(($row['total_harga']), 2, ",", ".") ?>">
-</div>
-<div class="user-form p-2">
-    <h2>Formulir Pembeli</h2>
-    <hr>
-    <label for="name"
-        class="form-label">Nama</label>
-    <input type="text" class="form-control mb-2"
-        name="name" id="name">
-    <label for="email"
-        class="form-label">Email</label>
-    <input type="email" class="form-control mb-2"
-        name="email" id="email">
-    <label for="no_hp" class="form-label">No
-        Ponsel Aktif</label>
-    <input type="text" class="form-control mb-2"
-        name="no_hp" id="no_hp">
-    <label for="address" class="form-label">Alamat
-        saat ini</label>
-    <input type="text" class="form-control mb-2"
-        name="address" id="address">
-    <label for="postal_code"
-        class="form-label">Kode Pos</label>
-    <input type="text" class="form-control mb-2"
-        name="postal_code" id="postal_code">
-    <button id="check_out_btn" type="button"
-        class="btn btn-primary mt-5">Checkouts</button>
-</div>
+    </ul>
+    <div class="d-flex mb-5 p-2">
+        <label for="total-transaction"
+            class="form-label me-5">Total</label>
+        <input id="total-transaction" type="text"
+            class="form-control" disabled
+            style="text-align: center;"
+            value="<?php echo "Rp" . number_format(($row['total_harga']), 2, ",", ".") ?>">
+    </div>
+    <div class="user-form p-2">
+        <h2>Formulir Pembeli</h2>
+        <hr>
+        <label for="name"
+            class="form-label">Nama</label>
+        <input type="text" class="form-control mb-2"
+            name="name" id="name">
+        <label for="email"
+            class="form-label">Email</label>
+        <input type="email" class="form-control mb-2"
+            name="email" id="email">
+        <label for="no_hp" class="form-label">No
+            Ponsel Aktif</label>
+        <input type="text" class="form-control mb-2"
+            name="no_hp" id="no_hp">
+        <label for="address" class="form-label">Alamat
+            saat ini</label>
+        <input type="text" class="form-control mb-2"
+            name="address" id="address">
+        <label for="postal_code"
+            class="form-label">Kode Pos</label>
+        <input type="text" class="form-control mb-2"
+            name="postal_code" id="postal_code">
+        <button id="check_out_btn" type="button"
+            class="btn btn-primary mt-5">Checkouts</button>
+    </div>
 <?php
 } else {
     echo 'Anda tidak memiliki barang belanja';
